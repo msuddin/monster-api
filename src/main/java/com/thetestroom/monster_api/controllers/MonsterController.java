f4988b02ea1374ec1daeb16fe8c0af89af6b4143
Mohammed Shahin Uddin <mo@thetestroom.com>
2025-03-07 20:12:48 +0000

    Updated MonsterController to use the MonsterService class instead of the MonsterRepository interface

--- src/main/java/com/thetestroom/monster_api/controllers/MonsterController.java
+++ src/main/java/com/thetestroom/monster_api/controllers/MonsterController.java
@@ -1,7 +1,7 @@
 package com.thetestroom.monster_api.controllers;
 
 import com.thetestroom.monster_api.models.Monster;
-import com.thetestroom.monster_api.repositories.MonsterRepository;
+import com.thetestroom.monster_api.services.MonsterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +14,23 @@ import java.util.Optional;
 @RequestMapping("/api/monsters")
 public class MonsterController {
 
+    private final MonsterService monsterService;
+
     @Autowired
-    private MonsterRepository monsterRepository;
+    public MonsterController(MonsterService monsterService) {
+        this.monsterService = monsterService;
+    }
 
     // Get all monsters
     @GetMapping
     public List<Monster> getAllMonsters() {
-        return monsterRepository.findAll();
+        return monsterService.getAllMonsters();
     }
 
     // Get a single monster by ID
     @GetMapping("/{id}")
     public ResponseEntity<Monster> getMonsterById(@PathVariable Long id) {
-        Optional<Monster> monster = monsterRepository.findById(id);
+        Optional<Monster> monster = monsterService.getMonsterById(id);
         return monster.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
@@ -34,30 +38,22 @@ public class MonsterController {
     // Create a new monster
     @PostMapping
     public ResponseEntity<Monster> createMonster(@RequestBody Monster monster) {
-        Monster savedMonster = monsterRepository.save(monster);
+        Monster savedMonster = monsterService.createMonster(monster);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedMonster);
     }
 
     // Update an existing monster
     @PutMapping("/{id}")
     public ResponseEntity<Monster> updateMonster(@PathVariable Long id, @RequestBody Monster monsterDetails) {
-        Optional<Monster> monsterOptional = monsterRepository.findById(id);
-        if (monsterOptional.isPresent()) {
-            Monster monster = monsterOptional.get();
-            monster.setName(monsterDetails.getName());
-            monster.setType(monsterDetails.getType());
-            Monster updatedMonster = monsterRepository.save(monster);
-            return ResponseEntity.ok(updatedMonster);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+        Optional<Monster> updatedMonster = monsterService.updateMonster(id, monsterDetails);
+        return updatedMonster.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     // Delete a monster
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMonster(@PathVariable Long id) {
-        if (monsterRepository.existsById(id)) {
-            monsterRepository.deleteById(id);
+        if (monsterService.deleteMonster(id)) {
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
